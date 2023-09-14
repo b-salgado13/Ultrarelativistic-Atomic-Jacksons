1f0ed1e06104525d0d925fabea668e49837f7cc4
Hugo Suárez Rangel <66455651+cosmicapple-svg@users.noreply.github.com>
2023-09-14 22:40:23 +0200

    Update Constantes.py
    Añadimos primeras constantes

--- Base/Constantes.py
+++ Base/Constantes.py
@@ -1,17 +1,32 @@
 """
 Este archivo contiene las constantes físicas de mayor relevancia que se utilizan a lo largo de los diferentes módulos.
-¿Se llaman vía scipy o no?
+¿Se llaman vía scipy o no? SI
 
 Propuestas de convenio para llamar constantes:
   1. Por su símbolo asociado.
-  2. Por su nombre literal.
+  2. Por su nombre literal.  -> Propongo esta opción
 
   Ejemplo: velocidad de la luz:
     1. La variable se declararía como "c"
     2. La variable se declararía como "velocidad_luz" o "velocidad_de_la_luz" o "v_luz", etc.
 
-Se declaran las variables po
+Este módulo está basado en el diccionario de scipy.constants.physical_constants:
+
+https://docs.scipy.org/doc/scipy/reference/constants.html#mathematical-constants
 """
 
-from scipy.constants import *
+from scipy.constants import physical constants
+from scipy.constants import pi
+#Constantes matemáticas
+pi = pi #xd
+
+#Constantes universales bien perras importantes
+hbarra         = physical_constants["reduced Planck constant"][0]
+carga_electron = physical_constants["electron charge"][0]
+
+
+#Masas
+masa_electron = physical_constants["electron mass"][0]
+masa_proton   = physical_constants["proton mass"][0]
+masa_neutron  = physical_constants["neutron mass"][0]
 
