526fc1350122b402185f635b3b2f0b64a161e833
Hugo Suárez Rangel <66455651+cosmicapple-svg@users.noreply.github.com>
2023-09-14 22:03:00 +0200

    Create Constantes.py
    Archivo que guardará todas las constantes que se puedan utilizar.

---
+++ Base/Constantes.py
@@ -0,0 +1,17 @@
+"""
+Este archivo contiene las constantes físicas de mayor relevancia que se utilizan a lo largo de los diferentes módulos.
+¿Se llaman vía scipy o no?
+
+Propuestas de convenio para llamar constantes:
+  1. Por su símbolo asociado.
+  2. Por su nombre literal.
+
+  Ejemplo: velocidad de la luz:
+    1. La variable se declararía como "c"
+    2. La variable se declararía como "velocidad_luz" o "velocidad_de_la_luz" o "v_luz", etc.
+
+Se declaran las variables po
+"""
+
+from scipy.constants import *
+
