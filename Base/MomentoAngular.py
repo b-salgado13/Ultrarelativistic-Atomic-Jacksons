852c0907ddae1fa1e64428daf39c87e2f81f62a7
Hugo Alexis Suárez Rangel <66455651+cosmicapple-svg@users.noreply.github.com>
2023-09-20 06:03:11 +0200

    Update MomentoAngular.py
    Se añade caso para l semi-entero, se añaden mensajes de error por para evaluar correctamente el valor de l (positivo y entero o semientero)

--- Base/MomentoAngular.py
+++ Base/MomentoAngular.py
@@ -5,7 +5,10 @@ Este archivo contiene las funciones correspondientes al momento angular. Algunas
   - Eigenfunciones 
 
 Pendiente:
-  -Añadir el caso de l semientero
+  -Añadir el caso de l semientero        [X]
+  - Prácticamente todas las funciones evalúan si l es entero o semientero, se podría definir una función IsSemiInteger para simplificar.
+
+  
 """
 
 import numpy as np
@@ -15,54 +18,77 @@ import numpy as np
 def MatrizJCuadrada(l):
     """
     Generador de la matriz de momento angular L^2 para valor de l.
-
-    TODO:
-      Añadir error para que se evalue que l sea entero o semientero positivo. 
     """
+    #Primero evaluamos que el momento angular sea entero o semientero positivo. 
+    if l%(1/2) !=0 or l<0:
+        raise ValueError("El momento angular debe ser entero o semientero positivo")
+      
     m_total = int(2*l + 1)
-    L2 = np.identity(m_total)
+    L2      = np.identity(m_total)
     return L2
   
 def MatrizJz(l):
     """
     Generador de la matriz de momento angular de L_Z para valor de l.
-
-    TODO:
-      Añadir error para que se evalue que l sea entero o semientero positivo. 
     """
-    m_valores = [i for i in range(-l, l+1, 1) ]
-    m_total   = 2*l + 1
-    Lz = np.zeros((m_total, m_total))
+  
+    #Primero evaluamos que el momento angular sea entero o semientero positivo. 
+    if l%(1/2) !=0 or l<0:
+        raise ValueError("El momento angular debe ser entero o semientero positivo")
+      
+    m_valores = [i for i in np.arange(-l, l+1, 1) ]
+    m_total   = int(2*l + 1)
+    Lz        = np.zeros((m_total, m_total))
 
     for i in range(m_total):
         Lz[i, i] = m_valores[i]
 
     return Lz
   
-def MatrizJMasMenos(l,p=True):
+def MatrizJMasMenos(l, p=True):
     """
     Generador de la matriz de subida/bajada de momento angular L_+/L_-
     """
-    m_valores = [i for i in range(-l, l+1, 1) ]
+    
+    #Primero evaluamos que el momento angular sea entero o semientero positivo. 
+    if l%(1/2) !=0 or l<0:
+        raise ValueError("El momento angular debe ser entero o semientero positivo")
+        
+    m_valores = [i for i in np.arange(-l, l+1, 1) ]
     eigenvalores_mas_menos = np.zeros(len(m_valores))
-
+    
     for j in range(len(m_valores)):
+        #Se evalua si se requiere el operador J+ o J-
         if p is True:
-            eigenvalores_mas_menos[j]= np.sqrt(l*(l+1)-m_valores[j]*(m_valores[j]+1))
+            eigenvalores_mas_menos[j]= np.sqrt( l*(l+1)-m_valores[j]*(m_valores[j]+1) )
         else:
-            eigenvalores_mas_menos[j]= np.sqrt(l*(l+1)-m_valores[j]*(m_valores[j]-1))
-    m_total   = 2*l + 1
+            eigenvalores_mas_menos[j]= np.sqrt( l*(l+1)-m_valores[j]*(m_valores[j]-1) )
+        
+    m_total   = int(2*l + 1)
     L_mas_menos = np.zeros((m_total, m_total))
 
     for i in range(m_total-1):
+        #Nuevamente se evalua si se requiere el operador J+ o J-
         if p is True:
             L_mas_menos[i, i+1] = eigenvalores_mas_menos[i]
         else:
             L_mas_menos[i+1, i] = eigenvalores_mas_menos[i+1]
+            
     return L_mas_menos
             
-def MatrizLx(l):
+def MatrizJx(l):
+    """
+    Generador de matriz Jx en base a los operadores escalera
+    """
     return (MatrizJMasMenos(l) + MatrizJMasMenos(l,False))/2
 
-def MatrizLy(l):
+def MatrizJy(l):
+    """
+    Generador de matriz Jy en base a los operadores escalera
+    """
     return complex(0,-1)*(MatrizJMasMenos(l) - MatrizJMasMenos(l,False))/2
+
+# En base a las funciones generadoras de matrices, podemos definir las matrices de Pauli
+pauli_x = 2*MatrizJx(1/2)
+pauli_y = 2*MatrizJy(1/2)
+pauli_z = 2*MatrizJz(1/2)
