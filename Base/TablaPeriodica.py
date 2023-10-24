476891eb1fd2aa1a881b6a72e46050912eddfafd
Bruno Ivan Salgado Molina <106989938+b-salgado13@users.noreply.github.com>
2023-10-24 06:00:47 +0000

    Pending changes exported from your codespace

--- Base/TablaPeriodica.py
+++ Base/TablaPeriodica.py
@@ -1,14 +1,113 @@
+"""
+Aquí se definen las masas y cargas de cada núcleo atómico y sus principales isótopos
+Estos se definen en términos de la masa del electrón ya que en física
+molecular la masa del electrón es la unidad de la masa
+La masa del electron es aproximadamente 5.485799*10^(-4) uma
+"""
 Atomo = {
-    "H":{
+    "H" : {
         1 : {
-            "masa" : 1
+            "masa" : 1837.152618
         },
         2 : {
-            "masa" : 2
+            "masa" : 3671.481583
         },
         3 : {
-            "masa" : 3
+            "masa" : 5497.921086
         },
         "carga" : 1
+    },
+    "He" : {
+        2 : {
+            "masa" : 3674.750022
+        },
+        3 : {
+            "masa" : 5497.884629
+        },
+        4 : {
+            "masa" : 7296.299044
+        },
+        5 : {
+            "masa" : 9136.421148
+        },
+        "carga" : 2
+    },
+    "Li" : {
+        5 : {
+            "masa" : 9137.301603
+        },
+        6 : {
+            "masa" : 10964.896818
+        },
+        7 : {
+            "masa" : 12789.391299
+        },
+        8 : {
+            "masa" : 14624.097601
+        },
+        9 : {
+            "masa" : 16454.831830
+        },
+        "carga" : 3
+    },
+    "Be" : {
+        7 : {
+            "masa" : 12791.077471
+        },
+        8 : {
+            "masa" : 14592.778554
+        },
+        9 : {
+            "masa" : 16428.204897
+        },
+        10 : {
+            "masa" : 18253.556136
+        },
+        "carga" : 4
+    },
+    "B" : {
+        8 : {
+            "masa" : 14627.963948
+        },
+        9 : {
+            "masa" : 16430.293928
+        },
+        10 : {
+            "masa" : 18252.466049
+        },
+        11 : {
+            "masa" : 20068.735657
+        },
+        12 : {
+            "masa" : 21900.824291
+        },
+        "carga" : 5
+    },
+    "C" : {
+        10 : {
+            "masa" : 18259.606303
+        },
+        11 : {
+            "masa" : 20072.612941
+        },
+        12 : {
+            "masa" : 21874.662195
+        },
+        13 : {
+            "masa" : 23703.66468
+        },
+        14 : {
+            "masa" : 25526.34721
+        },
+        15 : {
+            "masa" : 27362.64854
+        },
+        16 : {
+            "masa" : 29193.014545
+        },
+        17 : {
+            "masa" : 31030.263777
+        },
+        "carga" : 6
     }
 }
