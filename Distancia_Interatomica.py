d9dde00367125611ee6341f3b414310f161f1baa
Bruno Ivan Salgado Molina <106989938+b-salgado13@users.noreply.github.com>
2023-11-23 16:19:25 -0600

    Update Distancia_Interatomica.py

--- Distancia_Interatomica.py
+++ Distancia_Interatomica.py
@@ -7,18 +7,6 @@ import pandas as pd
 import Base.Constantes as const
 import Base.TablaPeriodica as elementos
 
-
-#MASAS DE LOS ELEMENTONS EN G/MOL
-Simbolos = [ 'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 
-          'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 
-          'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr']
-Masas_Atomicas =  [1.0079, 4.0026, 6.939, 9.0122, 10.8111, 12.011, 14.006, 15.999, 
-                     18.998, 20.183, 22.989, 24.312, 26.981, 28.086, 30.973, 
-                     32.084, 35.453, 39.948, 39.102, 40.08, 44.956, 47.90, 50.942, 
-                   51.966, 54.938, 55.847, 58.933, 58.71, 63.55, 65.54, 65.37,
-                   65.37, 69.72, 72.59, 72.922, 78.922, 78.96, 79.909, 83.80] 
-
-
 def iniciar(Simbolos, Masas_Atomicas):
   print("---------------------------------------")
   print("--- DISTANCIA INTER ATÓMICA ---\n--- A PARTIR DE FRENCUENCIA DE ABSORCION---")
